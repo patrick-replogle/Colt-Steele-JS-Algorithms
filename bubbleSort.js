8d28e932951a3413173b8d32c1268924939ff5a9
Patrick Replogle <50844285+patrick-replogle@users.noreply.github.com>
2020-02-19 16:57:02 -0800

    Create bubbleSort.js

---
+++ bubbleSort.js
@@ -0,0 +1,31 @@
+//non optimized version
+function bubbleSort(arr) {
+  for(let i = arr.length; i > 0; i--) {
+    for(let j = 0; j < i - 1; j++) { 
+      if(arr[j] > arr[j+1]) {
+        let temp = arr[j];
+        arr[j] = arr[j+1];
+        arr[j+1] = temp;
+      }
+    }
+  }
+  return arr;
+}
+
+//optimized version
+function bubbleSort(arr) {
+  let noSwaps;
+  for(let i = arr.length; i > 0; i--) {
+    noSwaps = true;
+    for(let j = 0; j < i - 1; j++) { 
+      if(arr[j] > arr[j+1]) {
+        let temp = arr[j];
+        arr[j] = arr[j+1];
+        arr[j+1] = temp;
+        noSwaps = false;
+      }
+    }
+    if(noSwaps) break;
+  }
+  return arr;
+}

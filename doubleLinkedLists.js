06b5c7de1e0e908ec2600e3cbfb2c80a6a16c40c
Patrick Replogle <50844285+patrick-replogle@users.noreply.github.com>
2020-02-20 19:47:14 -0800

    Update doubleLinkedLists.js

--- doubleLinkedLists.js
+++ doubleLinkedLists.js
@@ -126,6 +126,21 @@ class DoublyLinkedList {
     this.length--;
     return removedNode;
   }
+  reverse() {
+    let node = this.head;
+    this.head = this.tail;
+    this.tail = node;
+    
+    let next;
+    let prev = null;
+    for(let i = 0; i < this.length; i++) {
+      next = node.next;
+      node.next = prev;
+      prev = node;
+      node = next;
+    }
+    return this;
+  }
 }
 
 let list = new DoublyLinkedList;

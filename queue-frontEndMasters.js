1933e9578ce44fc466943f65d996e336ad2e1987
Patrick Replogle <50844285+patrick-replogle@users.noreply.github.com>
2020-04-05 14:33:12 -0700

    Create queue-frontEndMasters.js

---
+++ queue-frontEndMasters.js
@@ -0,0 +1,33 @@
+
+class Queue {
+
+  constructor() {
+    this._storage =  {};
+    this._length = 0;
+    this._headIndex = 0;
+  }
+  enqueue(value) {
+    this._storage[this._length  + this._headIndex] = value;
+    this._length++;
+  }
+  dequeue() {
+    if(!this._length) return undefined;
+    const firstVal = this._storage[this._headIndex];
+    delete this._storage[this._headIndex];
+    this._length--;
+    this._headIndex++;
+    return firstVal;
+  }
+  peek() {
+    if(!this._length) return undefined;
+    return this._storage[this._headIndex];
+  }
+}
+
+const myQ = new Queue()
+myQ.enqueue('zero')
+myQ.enqueue('one')
+myQ.enqueue('two')
+myQ.dequeue()
+console.log(myQ.peek())
+console.log(myQ)
